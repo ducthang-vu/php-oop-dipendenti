7906f94461cb84ea36bb4434ab4c8f152bedad5f
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-08 13:41:39 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,10 @@
+console.log('main.js is working')
+console.log($)
+
+/*
+var source = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+
+var context = { title: "My New Post", body: "This is my first post!" };
+var html = template(context);
+*/
